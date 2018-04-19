2525a4d6061961482efa5b3638a4e8ea5fbfec1c
mingooddev <mingood92@gmail.com>
2018-04-20 00:18:57 +0900

    Update Standard IO case 1: first try case 2: System.out.print case 3: StringBuilder

---
+++ src/algorithm/StandardInOut/StringBuilder/Ex01.java
@@ -0,0 +1,22 @@
+package algorithm.StandardInOut.StringBuilder;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        String[] unit = { "", "십", "백", "천", "만" };
+        String[] number = { "", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구" };
+
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < args[0].length(); ++i) {
+            int current = Character.getNumericValue(args[0].charAt(i));
+            if(current == 0) continue;
+            builder.append((current != 1 ||
+                            args[0].length() == 1 ||
+                            (i+1) == args[0].length() ? number[current] : ""))
+                    .append(unit[args[0].length() - (i+1)]);
+        }
+        System.out.println(builder.toString());
+    }
+
+}
