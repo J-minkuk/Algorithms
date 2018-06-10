8a115a2d26632155afe1417ffc598927f757867c
mingooddev <mingood92@gmail.com>
2018-06-10 19:20:04 +0900

    Add Factorial
    Update Fibonacci

--- src/programmers/level1/Fibonacci.java
+++ src/programmers/level1/Fibonacci.java
@@ -1,32 +1,34 @@
 package programmers.level1;
 
-class Fibonacci {
+public class Fibonacci {
 
-//    public long fibonacci(int num) {
-//        long answer = 0;
-//        if (num <= 1) return num;
-//        else answer = fibonacci(num - 1) + fibonacci(num - 2);
-//        return answer;
-//    }
+  public long recursiveFibonacci(int num) {
+    long answer = 0;
+    if (num <= 1) return num;
+    else answer = recursiveFibonacci(num - 1) + recursiveFibonacci(num - 2);
+    return answer;
+  }
 
-    // 아래는 테스트로 출력해 보기 위한 코드입니다.
-    public static void main(String[] args) {
-        Fibonacci c = new Fibonacci();
-        int testCase = 3;
-        System.out.println(c.fibonacci(testCase));
+  public long loopFibonacci(int num) {
+    long answer = 0;
+    int count = 1;
+    long a = 0, b = 1;
+
+    while (count < num) {
+      answer = a + b;
+      a = b;
+      b = answer;
+      count++;
     }
+    return answer;
+  }
 
-    public long fibonacci(int num) {
-        long answer = 0;
-        int count = 1;
-        long a = 0, b = 1;
+  // 아래는 테스트로 출력해 보기 위한 코드입니다.
+  public static void main(String[] args) {
+    Fibonacci c = new Fibonacci();
+    int testCase = 10;
+    System.out.println(c.recursiveFibonacci(testCase));
+    System.out.println(c.loopFibonacci(testCase));
+  }
 
-        while(count < num) {
-            answer = a + b;
-            a = b;
-            b = answer;
-            count++;
-        }
-        return answer;
-    }
 }
