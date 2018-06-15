68cbe3d6cce68394159c59cee3bfe8c0099575db
mingooddev <mingood92@gmail.com>
2018-06-15 13:58:29 +0900

    Add Numbers & ReverseString

---
+++ src/com/algorithm/list/numbers/Numbers.java
@@ -0,0 +1,38 @@
+package com.algorithm.list.numbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 숫자가 들어있는 리스트 만들기
+ */
+public class Numbers {
+
+  static List<Integer> list = new ArrayList<>();
+
+  static void makeList1(int num) {
+    if (num < 0) return;
+    else {
+      list.add(num--);
+      makeList1(num);
+    }
+  }
+
+  static List<Integer> makeList2(int num) {
+    List<Integer> list2 = new ArrayList<>();
+    for (int i = 0; i <= num; ++i) {
+      list2.add(i);
+    }
+    return list2;
+  }
+
+  public static void main(String[] args) {
+    makeList1(9);
+    for (int i = 0; i < list.size(); ++i) {
+      System.out.print(list.get(i)+ " ");
+    }
+    System.out.println();
+    System.out.println(makeList2(9).toString());
+  }
+
+}
---
+++ src/com/algorithm/string/reverse/ReverseString.java
@@ -0,0 +1,30 @@
+package com.algorithm.string.reverse;
+
+import java.util.Stack;
+
+/**
+ * 문자열 거꾸로 만들기
+ */
+public class ReverseString {
+
+  static String reverseString(String s) {
+    char[] chars = s.toCharArray();
+    StringBuilder sb = new StringBuilder();
+    Stack<String> stack = new Stack<>();
+
+    for (char c : chars) {
+      stack.push(Character.toString(c));
+    }
+
+    while (!stack.empty()) {
+      sb.append(stack.pop());
+    }
+
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+    String str = "MinGOOD";
+    System.out.println(reverseString(str));
+  }
+}
