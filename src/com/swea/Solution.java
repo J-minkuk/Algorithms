9011531f29f7a243a50df121aae8fc9de25fff9f
MinGOODdev <mingood92@gmail.com>
2018-12-30 02:42:39 +0900

    SWEA: 1204, 1206

---
+++ src/com/swea/Solution.java
@@ -0,0 +1,10 @@
+package com.swea;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+  public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  }
+}
