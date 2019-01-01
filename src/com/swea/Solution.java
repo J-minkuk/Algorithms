be022932a2469ddf021c4f11d6a57535a83d555b
MinGOODdev <mingood92@gmail.com>
2019-01-02 00:24:27 +0900

    SWEA: 1211

--- src/com/swea/Solution.java
+++ src/com/swea/Solution.java
@@ -1,13 +1,15 @@
 package com.swea;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Solution {
-  public static void main(String[] args) throws FileNotFoundException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedReader br1 = new BufferedReader(new FileReader(""));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br = new BufferedReader(new FileReader(""));
+
+    int testCaseCount = Integer.parseInt(br.readLine());
+    for (int testCaseNum = 1; testCaseNum < testCaseCount; ++testCaseNum) {
+
+    }
   }
 }
