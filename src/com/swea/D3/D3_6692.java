622da28d0cf875ae3eb6ba46e1d2fc5f6e71d8b5
mingooddev <mingood92@gmail.com>
2019-01-15 16:38:50 +0900

    SWEA: 1215, 5549, 6692, 1218

---
+++ src/com/swea/D3/D3_6692.java
@@ -0,0 +1,20 @@
+package com.swea.D3;
+
+import java.util.Scanner;
+
+public class D3_6692 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for (int t = 1; t <= T; ++t) {
+      int N = sc.nextInt();
+      double result = 0;
+      for (int i = 0; i < N; ++i) {
+        double p = sc.nextDouble();
+        double x = sc.nextDouble();
+        result += p * x;
+      }
+      System.out.printf("#%d %.6f", t, result);
+    }
+  }
+}
---
+++ src/com/swea/D4/D4_1218.java
@@ -0,0 +1,36 @@
+package com.swea.D4;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class D4_1218 {
+  static char[] QS = {'(', ')', '[', ']', '{', '}', '<', '>'};
+  public static void main(String[] args) throws IOException {
+    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br = new BufferedReader(new FileReader("C:\\intelliJ_workspaces\\Algorithm\\src\\com\\swea\\D4\\testcase\\1218input.txt"));
+    for (int t = 1; t <= 10; ++t) {
+      br.readLine();
+      String s = br.readLine();
+
+      Map<Character, Integer> map = new HashMap<>();
+      int length = s.length();
+      for (int i = 0; i < length; ++i) {
+        char c = s.charAt(i);
+        if (map.get(c) == null) map.put(c, 1);
+        else map.put(c, map.get(c) + 1);
+      }
+
+      boolean flag = true;
+      for (int i = 0; i < QS.length - 1; i = i + 2) {
+        if (map.get(QS[i]) != map.get(QS[i + 1])) {
+          flag = false;
+          break;
+        }
+      }
+
+      if (flag) System.out.println("#" + t + " " + 1);
+      else System.out.println("#" + t + " " + 0);
+    }
+  }
+}
