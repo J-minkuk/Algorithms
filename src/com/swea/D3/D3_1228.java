66cd7613dce69aa097ec19417197ae1b2878e2c2
MinGOODdev <mingood92@gmail.com>
2019-01-28 23:43:42 +0900

    SWEA: 1228
    - LinkedList 활용

--- src/com/swea/D3/D3_1228.java
+++ src/com/swea/D3/D3_1228.java
@@ -2,29 +2,41 @@ package com.swea.D3;
 
 import java.io.*;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class D3_1228 {
   public static void main(String[] args) throws IOException {
     Scanner sc1 = new Scanner(System.in);
-    Scanner sc = new Scanner(new FileReader("C:\\intelliJ_workspaces\\Algorithm\\src\\com\\swea\\D3\\testcase\\1228input.txt"));
-    for (int t = 1; t <= 1; ++t) {
+    Scanner sc = new Scanner(new FileReader("D:\\sts_projects\\Algorithm\\src\\com\\swea\\D3\\testcase\\1228input.txt"));
+    for (int t = 1; t <= 10; ++t) {
       int N = sc.nextInt(); sc.nextLine();
       String[] str = sc.nextLine().split(" ");
 
-      LinkedList<String> list = new LinkedList<>();
+      LinkedList<Integer> list = new LinkedList<>();
       for (int i = 0; i < N; ++i) {
-        list.add(str[i]);
+        list.add(Integer.parseInt(str[i]));
       }
 
       int commandC = sc.nextInt(); sc.nextLine();
       for (int i = 0; i < commandC; ++i) {
+        LinkedList<Integer> tempList = new LinkedList<>();
+
         if(sc.next().equals("I")) {
           int idx = sc.nextInt();
           int count = sc.nextInt();
-          System.out.println(sc.nextInt());
+          for (int j = 0; j < count; ++j) {
+            tempList.add(sc.nextInt());
+          }
+          list.addAll(idx, tempList);
         }
       }
+      List<Integer> result = list.subList(0, 10);
+      System.out.print("#" + t + " ");
+      for (int value : result) {
+        System.out.print(value + " ");
+      }
+      System.out.println();
     }
   }
 }
