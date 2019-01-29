00714da9346d3e7e241a7af6721d855653ffa05f
MinGOODdev <mingood92@gmail.com>
2019-01-30 00:38:36 +0900

    SWEA: 1233

---
+++ src/com/swea/D4/D4_1233.java
@@ -0,0 +1,32 @@
+package com.swea.D4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class D4_1233 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int T = 10;
+    for (int t = 1; t <= T; ++t) {
+      int N = Integer.parseInt(br.readLine());
+      boolean flag = true;
+
+      for (int i = 1; i <= N / 2; ++i) {
+        String[] str = br.readLine().split(" ");
+        String value = str[1];
+        if (!(value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/"))) flag = false;
+      }
+
+      for (int i = N / 2 + 1; i <= N; ++i) {
+        String[] str = br.readLine().split(" ");
+        String value = str[1];
+        if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) flag = false;
+      }
+
+      if (flag) System.out.println("#" + t + " " + 1);
+      else System.out.println("#" + t + " " + 0);
+    }
+  }
+}
--- src/com/swea/Solution.java
+++ src/com/swea/Solution.java
@@ -1,15 +1,8 @@
 package com.swea;
 
-import java.io.*;
-
 public class Solution {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
-    BufferedReader br = new BufferedReader(new FileReader(""));
-
-    int T = Integer.parseInt(br.readLine());
-    for (int t = 1; t <= T; ++t) {
-
-    }
+  public static void main(String[] args) {
+    System.out.println(Character.getNumericValue('-'));
+    System.out.println(Character.getNumericValue(' '));
   }
 }
