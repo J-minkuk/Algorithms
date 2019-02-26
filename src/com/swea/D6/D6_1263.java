7c7f1694730df40c13a0f119ab499b116e16049e
MinGOODdev <mingood92@gmail.com>
2019-02-27 01:29:46 +0900

    SWEA: 1263

--- src/com/swea/D6/D6_1263.java
+++ src/com/swea/D6/D6_1263.java
@@ -4,27 +4,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class D6_1263 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int T = Integer.parseInt(br.readLine());
-    for (int t = 1; t <= 1; ++t) {
-      String[] str = br.readLine().split(" ");
-      int N = Integer.parseInt(str[0]);
-      int[][] NWK = new int[N][N];
-      int idx = 1;
+    for (int t = 1; t <= T; ++t) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int N = Integer.parseInt(st.nextToken());
+      int NWT[][] = new int[N][N];
+      int[] cc = new int[N];
+
       for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-          NWK[i][j] = Integer.parseInt(str[idx++]);
+          NWT[i][j] = Integer.parseInt(st.nextToken());
+          if (i != j && NWT[i][j] == 0) NWT[i][j] = 9999999;
         }
       }
 
-      // 절반만 신경쓰면됨
-      for (int i = 0; i < N; ++i) {
-        System.out.println(Arrays.toString(NWK[i]));
+      for (int v = 0; v < N; ++v) {
+        for (int i = 0; i < N; ++i) {
+          for (int j = 0; j < N; ++j) {
+            NWT[i][j] = Math.min(NWT[i][j], NWT[i][v] + NWT[v][j]);
+          }
+        }
       }
 
+      int result = Integer.MAX_VALUE;
+      for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+          cc[i] += NWT[i][j];
+        }
+        result = Math.min(result, cc[i]);
+      }
+      System.out.println("#" + t + " " + result);
     }
   }
 }
